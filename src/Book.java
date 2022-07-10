0e6b489e709300b11dcc1f54db43881bf192e03c
irekhusainov <i@khusainovirek>
2022-07-10 16:11:49 +0300

    final

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Book {
+import java.security.PublicKey;
+import java.security.cert.PKIXCertPathBuilderResult;
+
+public class Book {
+        private final String nameBook;
+        private final Author author;
+        private int yearPublishing;
+    public Book(String nameBook,Author author, int yearPublishing) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearPublishing = yearPublishing;
+
+    }
+    public String getNameBook(){
+        return this.nameBook;
+    }
+    public Author getAuthor(){
+        return this.author;
+    }
+    public int getYearPublishing(){
+        return this.yearPublishing;
+    }
+    public void setYearPublishing(int new_year){
+        this.yearPublishing = new_year;
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Person {
+import java.security.PublicKey;
+
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return this.name;
+
+    }
+    public int getAge() {
+        return this.age;
+
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
